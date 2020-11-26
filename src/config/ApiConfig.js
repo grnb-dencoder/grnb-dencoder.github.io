322490250925e1ae24dd29f120b6c8cc7b5abebb
glebremiov <remniov.work@gmail.com>
2020-11-26 13:53:12 +0300

    Adjust env logic

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,2 +1,9 @@
-// export const API_BASE = 'http://localhost:8080'
-export const API_BASE = 'https://dencoder.herokuapp.com'
+const _withMode = (dev, prod) => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    return dev()
+  } else {
+    return prod()
+  }
+}
+
+export const API_BASE = process.env.REACT_APP_API_BASE || _withMode(() => process.env.REACT_APP_API_BASE_DEVELOPMENT, process.env.REACT_APP_API_BASE_PRODUCTION)
